d02eb8063a24787a347fdefe8c2c926d3bec8ae8
Aljoscha Nyang <a.nyang@gmx.de>
2023-10-30 15:46:46 +0100

    Create CharacterForm and add react-router-dom dependency, configure routes for gallery and form

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,16 @@
 import './App.css';
 import CharacterCardsList from "./components/CharacterCardsList.tsx";
+import {Route, Routes} from "react-router-dom";
+import CharacterForm from "./components/CharacterForm.tsx";
 
 function App() {
-    return <CharacterCardsList />
+    return (
+        <Routes>
+            <Route path={""} element={<CharacterCardsList />}/>
+            <Route path={"/add"} element={<CharacterForm />}/>
+
+        </Routes>
+    )
 }
 
 export default App
---
+++ src/components/CharacterForm.tsx
@@ -0,0 +1,5 @@
+function CharacterForm() {
+    return <h2>Hello World</h2>
+}
+
+export default CharacterForm;

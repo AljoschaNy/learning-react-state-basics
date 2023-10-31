4e8c6546d764ae860710fb22d1d036479fb0c6c3
Aljoscha Nyang <a.nyang@gmx.de>
2023-10-31 12:52:59 +0100

    Add previous and next buttons and change get characters from api

---
+++ src/components/Pagination.tsx
@@ -0,0 +1,25 @@
+/*
+type Props = {
+    onClick: () => void,
+    actualSite: number,
+    setActualSite: (value:number) => void,
+}
+
+function Pagination(props:Props) {
+    function handleClickNext() {
+        props.setActualSite(props.actualSite++);
+    }
+
+    function handleClickPrevious() {
+        props.setActualSite(props.actualSite--);
+    }
+
+    return (
+        <>
+            <button onClick={props.onClick}>Previous</button>
+            <button onClick={props.onClick}>Next</button>
+        </>
+    );
+}
+
+export default Pagination;*/
